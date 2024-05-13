4e0cdbba444b23d5cabcd2d436fb9afa0c55a0c9
acfakhri <amrifakhrizul@gmail.com>
2024-05-13 22:25:12 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  './src/**/*.html',
+  './src/**/*.js',
+  './src/**/*.jsx',
+  './src/**/*.ts',
+  './src/**/*.tsx',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
