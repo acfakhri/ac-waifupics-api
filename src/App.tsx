a6858bc854085690ed5eedd374787a7a932d44e3
acfakhri <amrifakhrizul@gmail.com>
2024-05-14 19:24:05 +0700

    Page error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import TrendingList from './pages/Trending/TrendingList';
 import NotFound from './pages/NotFound';
