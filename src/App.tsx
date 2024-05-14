39e32fe5e2cd066259720d8ee756c15460ac8c04
acfakhri <amrifakhrizul@gmail.com>
2024-05-14 19:11:14 +0700

    Page error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import TrendingList from './pages/Trending/TrendingList';
 import NotFound from './pages/NotFound';
@@ -11,17 +11,17 @@ import PopularList from './pages/Popular/PopularList';
 const App: React.FC = () => {
   return (
     <div>
-    <Header />
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/trending" element={<TrendingList />} />
-        <Route path="/tags" element={<Taglist />}/>
-        <Route path="/popular" element={<PopularList />}/>       
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-    <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/trending" element={<TrendingList />} />
+          <Route path="/tags" element={<Taglist />} />
+          <Route path="/popular" element={<PopularList />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
